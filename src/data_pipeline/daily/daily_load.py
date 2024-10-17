646c3cf7a485da1f353e46f87b45d010038f196a
VenkataDokku <vdokku@zilliontechnologies.com>
2024-10-17 17:57:59 -0400

    Added Changes to the Daily Load.

--- src/data_pipeline/daily/daily_load.py
+++ src/data_pipeline/daily/daily_load.py
@@ -1,40 +1,12 @@
-import string
-from awsglue.context import GlueContext
-from awsglue.job import Job
-from pyspark.context import SparkContext
-import re
 import boto3
-from pyspark.sql.dataframe import DataFrame
-import pyspark.sql.functions as f
-from functools import reduce
-from collections import Counter
-from src.util.helper import (read_parameter_query, read_ingestion_conf, read_table)
-from src.util.db_utils import insert_record, update_record
-from src.util.job_tracking import INSERT_SQL, UPDATE_SQL
-import sys
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
-from awsglue.transforms import *
-from pyspark.context import SparkContext
-import time
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from getpass import getpass
-from pyspark.sql.types import *
-import datetime
-import string
-import re
-import yaml
-import boto3
-from awsglue.context import GlueContext
-from pyspark.sql.dataframe import DataFrame
 
+from awsglue.job import Job
+from pyspark.context import SparkContext
+from pyspark.sql.functions import *
+from awsglue.context import GlueContext
 import pyspark.sql.functions as f
-from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql.functions import col, array, when, array_remove
+from src.util.helper import (read_parameter_query, load_data_2_rds, read_table, write_data_2_s3)
 
 
 s3 = boto3.resource('s3')
@@ -73,27 +45,22 @@ def process_daily_load():
     daily_table_df_select.show(100, truncate=False)
 
     print(daily_table_df_select.count())
-    print("Hello 456")
     stage_df_select.show(100, truncate=False)
     daily_table_df_select.show(100, truncate=False)
-    print("Hello 82374847")
 
     diff_df_left_anti=stage_df_select.join(daily_table_df_select, stage_df_select["concatenated_cols"] == daily_table_df_select["concatenated_cols"], "leftanti")
-    print(" After After 7777777777777 BEFORE &&&&&&&&&&&&&&&&&&&&&&&& * ")
+    print(" DataFrame Left Anti Join. To Get the New/Individual Records. ")
     print(diff_df_left_anti.count())
     diff_df_left_anti.show(100, truncate=False)
     diff_df_left_anti=diff_df_left_anti.withColumn("flag_del", lit("N")).withColumn("last_updt_dt", lit("9999-99-99"))
-    print(" After After 7777777777777 AFTER &&&&&&&&&&&&&&&&&&&&&&&& * ")
+    print(" New/Individual Records with Flag indicator and Last Update Timestamp. ")
     print(diff_df_left_anti.count())
     diff_df_left_anti.show(100, truncate=False)
 
     diff_df_left=stage_df_select.join(daily_table_df_select, (stage_df_select.concatenated_cols == daily_table_df_select.concatenated_cols), "left")
-    print(" Left Join 1 ")
-    print(diff_df_left.count())
-    diff_df_left.show(100, truncate=False)
     diff_df_left = diff_df_left.withColumn("is_due_amt_updated", when(col("bal_amt_stage") != col("bal_amt_daily"), "YES")
                                            .otherwise("NO"))
-    print(" Left Join 22222 ")
+    print(" For the Common Records, what ROWS were changed in amount ? ")
     print(diff_df_left.count())
     diff_df_left.show(100, truncate=False)
 
@@ -101,8 +68,6 @@ def process_daily_load():
     daily_expired_df=diff_df_left.filter(diff_df_left['is_due_amt_updated'] == lit("YES")).select("company_name_daily","level_name_daily","acc_id_daily","acc_name_daily","bal_dt_daily","bal_amt_daily")
 
 
-    print(daily_updated_df.count())
-    daily_updated_df.show(100, truncate=False)
     daily_updated_df=(daily_updated_df.withColumnRenamed('company_name_stage', 'company_name')
                       .withColumnRenamed('level_name_stage', 'level_name')
                       .withColumnRenamed('acc_id_stage', 'acc_id')
@@ -111,6 +76,10 @@ def process_daily_load():
                       .withColumnRenamed('bal_amt_stage', 'bal_amt')
                       .withColumn("flag_del", lit("N"))
                       .withColumn("last_updt_dt", lit("9999-99-99")))
+
+    print(" Updated Records & Details. ")
+    print(daily_updated_df.count())
+    daily_updated_df.show(100, truncate=False)
 
     daily_expired_df=(daily_expired_df.withColumnRenamed('company_name_daily', 'company_name')
                       .withColumnRenamed('level_name_daily', 'level_name')
@@ -121,11 +90,18 @@ def process_daily_load():
                       .withColumn("flag_del", lit("Y"))
                       .withColumn("last_updt_dt", f.date_format(f.current_timestamp(), 'yyyy-MM-dd')))
 
-    print("***************************** Finsh DF before writing to DB ")
-    print(daily_updated_df.count())
-    daily_updated_df.show(100, truncate=False)
-    print("***************************** Finsh DF before writing to DB ")
+    print(" Expired Records & Details. ")
     print(daily_expired_df.count())
     daily_expired_df.show(100, truncate=False)
+
+    print("***************************** Finsh DF before writing to DB ")
+    print(" Write Data to DB and Write data to S3 File.")
+    load_data_2_rds(daily_updated_df, conf["table_bronze_layer"])
+    load_data_2_rds(daily_expired_df, conf["table_bronze_layer"])
+    print("************ DB is updated with updated data****************")
+    print("************* Data is written to S3 *****************")
+    write_data_2_s3(glueContext, daily_updated_df, conf["s3_destination"])
+    write_data_2_s3(glueContext, daily_expired_df, conf["s3_destination"])
+
     print("---------------------Daily Job Completed---------------------")
 
